4fd6a8ce8a7b2ce7e3029f9a6fa88c38da59ee99
Ragul Thirumoorthy <ragulthirumoorthy.er@gmail.com>
2026-03-25 20:55:16 +0530

    Add User entity

---
+++ User.java
@@ -0,0 +1,19 @@
+package com.budgetwise.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+    // Getters & Setters
+}
